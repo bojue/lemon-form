71ec5ab37021678de36848bd9d7d265134738256
maliang <maliang@sharetek.com.cn>
2025-03-30 12:33:16 +0800

    delete dist

--- dist/assets/AboutView-mNe5H2wU.js
+++
@@ -1,1 +0,0 @@
-import{j as o,c as s,b as t,o as a}from"./index-BOB6v2i8.js";const n={},c={class:"about"};function r(i,e){return a(),s("div",c,e[0]||(e[0]=[t("h1",null,"This is an about page",-1)]))}const l=o(n,[["render",r]]);export{l as default};
